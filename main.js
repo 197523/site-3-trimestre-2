6d264fa6b9a73b5d3a30e49e91abba97e3469773
Lucas Bilecki <105056272+197523@users.noreply.github.com>
2024-10-24 10:47:11 -0300

    ffs

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ function mostraResposta(cartao_conteudo){
     const resposta = cartao_conteudo.querySelector('.cartao-resposta')
 
 if(resposta.style.display === 'none'){
-     resposta.style.display == 'block'
+     resposta.style.display = 'block'
     }
     else{
-        resposta.style.display == 'none'
+        resposta.style.display = 'none'
     }
 }
